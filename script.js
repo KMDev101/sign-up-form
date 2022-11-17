d5d2c349113939849d41c324b97c5738346f451d
DevS <shobujislam5040@gmail.com>
2022-11-17 13:25:05 +0600

    signup-form
    signup-form with html, css, javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var container = document.querySelector(".container");
+
+var signUpBtn = document.querySelector(".green-bg button");
+
+signUpBtn.addEventListener("click", () => {
+    
+    container.classList.toggle("change");
+
+});
