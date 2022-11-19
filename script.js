2b31697aeb18b62702b4f2fedb141c4ec581eed1
DevS <shobujislam5040@gmail.com>
2022-11-19 20:13:34 +0600

    menu toggle
    menu toggle

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var container = document.querySelector(".container");
 var signUpBtn = document.querySelector(".green-bg button");
 
 signUpBtn.addEventListener("click", () => {
-    
+
     container.classList.toggle("change");
 
 });
